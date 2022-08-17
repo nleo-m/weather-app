7bb220d4e0f368145cd1b3d15fb94f4f67a7eb27
Liandra Marques <liandra.mrq@gmail.com>
2022-08-17 19:53:11 -0300

    Add styling and icons

--- src/components/Weather.vue
+++ src/components/Weather.vue
@@ -3,19 +3,70 @@
 </script>
 
 <template>
-    <section class="wrapper" :style="{backgroundImage: `var(${this.dayPeriod})`}">
+    <section class="wrapper" :style="{backgroundImage: `var(${dayPeriod})`, color: dayPeriod == '--night' || dayPeriod == '--dawn' ? 'var(--light)' : 'var(--dark)'}">
         <img />
-        <p>{{timezone}}</p>
-        <p class="temperature">{{current?.temp ? current.temp + 'º' : '0º'}}</p>
-        <p>{{current ? firstLetterCapital(current.weather[0].description) : null}}</p>
-        <p>{{currentTime}}</p>
 
-        <p v-for="error in errors">{{error}}</p>
+        <div class="timezone">
+          <p>{{currentTime}}</p>
+          <p>{{timezone}}</p>
+        </div>
+
+        <div v-if="current" class="weather-info">
+          <img :src="'icons/' + current?.weather[0].icon + '.png'" />
+          <p>{{current.temp ? current.temp + 'º' : '0º'}}</p>
+          <p class="description">{{firstLetterCapital(current.weather[0].description)}}</p>
+        </div>
+
+        <div class="footer-info">
+          <div>
+            <font-awesome-icon icon="fa-solid fa-wind" />
+            <span>{{current?.wind_speed}}m/s</span>
+          </div>
+          <div>
+            <font-awesome-icon icon="fa-solid fa-droplet" />
+              <span>{{current?.humidity}}%</span>
+          </div>
+          <div>
+            <font-awesome-icon icon="fa-solid fa-sun" />
+            <span>{{current?.uvi}}</span>
+          </div>
+        </div>
+
+        <p class="error" v-for="error in errors">{{error}}</p>
+
+
+        <div v-if="hourly && daily" class="weather-panel">
+          <div class="switch-wrapper">
+            <div class="switch" v-on:click="handleSwitch()">
+              <p :class="!panelSwitch ? 'active' : null">Hourly</p>
+              <p :class="panelSwitch ? 'active' : null">Daily</p>
+            </div>
+          </div>
+
+          <div class="predictions hourly" v-if="!panelSwitch">
+            <div class="detail" v-for="predict in hourly">
+              <span>{{getFormattedTime(unixTimestampConverter(predict.dt))}}</span>
+              <img :src="'icons/' + predict.weather[0].icon + '.png'" />
+              <span>{{predict.temp}}º</span>
+            </div>
+          </div>
+
+          <div class="predictions daily" v-if="panelSwitch">
+            <div class="detail" v-for="predict in daily">
+              <span>{{getFormattedDate(unixTimestampConverter(predict.dt))}}</span>
+              <img :src="'icons/' + predict.weather[0].icon + '.png'" />
+              <span>{{predict.temp.day}}º</span>
+            </div>
+          </div>
+        </div>
+
     </section>
 </template>
 
 <style scoped>
+    @import url('https://fonts.googleapis.com/css2?family=Roboto&display=swap');
     @import './weather.css';
+    @import './responsive.css';
 </style>
 
 <script>
@@ -29,6 +80,9 @@
         current: null,
         hourly: null,
         daily: null,
+        sunriseIn: null,
+        sunsetIn: null,
+        panelSwitch: false,
         errors: []
       }
     },
@@ -40,6 +94,11 @@
         this.current = data.current;
         this.hourly = data.hourly;
         this.daily = data.daily;
+        const sunset =  this.unixTimestampConverter(data.current.sunset);
+        const sunrise = this.unixTimestampConverter(data.current.sunrise);
+        this.sunriseIn = sunrise.getHours();
+        this.sunsetIn = sunset.getHours();
+        console.log(this.sunsetIn, this.sunriseIn);
       },
 
       failGeolocation(error) {
@@ -51,38 +110,57 @@
         return str[0].toUpperCase() + str.replace(/^./, '');
       },
 
-      getTime() {
-        const aditionalZero = (value) => {
-          return value < 10 ? '0' + value : value;
-        };
-
+      getCurrentTime() {
         const date = new Date();
         const currHour = date.getHours();
         const currMinutes = date.getMinutes();
 
         this.dayPeriod = this.getDayPeriod(currHour);
 
-        this.currentTime = aditionalZero(currHour) + ':' + aditionalZero(currMinutes);
+        this.currentTime = this.getFormattedTime(date);
+      },
+
+      unixTimestampConverter(timestamp) {
+        return new Date(timestamp * 1000);
+      },
+
+      aditionalZero(value) {
+        return value < 10 ? '0' + value : value;
+      },
+
+      getFormattedTime(timestamp) {
+        return this.aditionalZero(timestamp.getHours()) + ':' + this.aditionalZero(timestamp.getMinutes());
+
+      },
+
+      getFormattedDate(timestamp) {
+        const days = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado'];
+
+        return days[timestamp.getDay()];
       },
       
       getDayPeriod(hour) {
-          if (hour >= 5 && hour < 8 )
+          if (hour >= this.sunriseIn && hour < this.sunriseIn + 2 )
             return '--dawn';
-          else if (hour >= 8 && hour < 12)
+          else if (hour >= this.sunriseIn + 2 && hour < 12)
             return '--morning';
-          else if (hour >= 12 && hour < 17)
+          else if (hour >= 12 && hour < this.sunsetIn)
             return '--afternoon';
-          else if (hour >= 17 && hour < 20)
+          else if (hour >= this.sunsetIn && hour < this.sunsetIn + 2)
             return '--evening';
           else
             return '--night';
       },
+
+      handleSwitch(){
+        return this.panelSwitch = !this.panelSwitch;
+      }
     },
 
     
     mounted () {
       navigator.geolocation.watchPosition(this.getWeather, this.failGeolocation);
-      setInterval(this.getTime, 1000);
+      setInterval(this.getCurrentTime, 1000);
     }
   }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,16 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faWind, faDroplet, faSun } from '@fortawesome/free-solid-svg-icons';
+
+library.add(faWind);
+library.add(faDroplet);
+library.add(faSun);
+
 import './assets/main.css'
 
-createApp(App).mount('#app')
+createApp(App)
+    .component('font-awesome-icon', FontAwesomeIcon)
+    .mount('#app')
